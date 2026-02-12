19048644beed5995b6919a77d27bd08706e7dd4a
Sal2991 <clongsong@gmail.com>
2026-02-12 18:42:29 -0500

    blank slate

--- src/main.cc
+++ src/main.cc
@@ -3,22 +3,6 @@
 
 int main() {
   std::println("Hello!");
-  std::cout << "Type a number1: ";
-  int x;
-  std::cin >> x;
-  std::cout << "Your number is: " << x << std::endl;
-  system("pause");
-  if (x > 0) {
-    std::cout << "Your number is positive." << std::endl;
-  } else if (x < 0) {
-    std::cout << "Your number is negative." << std::endl;
-  } else {
-    std::cout << "Your number is zero." << std::endl;
-  }
-  if (x % 2 == 0) {
-    std::cout << "Your number is even." << std::endl;
-  } else {
-    std::cout << "Your number is odd." << std::endl;
-  }
+  std::cout << "World!" << std::endl;
   return 0;
 }
