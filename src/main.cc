3b91fb524349d1cc3ab5e8ac0384ba6036796369
Sal2991 <clongsong@gmail.com>
2026-02-12 18:12:05 -0500

    initial commit

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,27 @@
+#include <iostream>
 #include <print>
+// cd build/
+// then save, then cmake —build .
+// then .\main.exe
 
 int main() {
-  std::println("Hello, World!");
+  std::println("Hello!");
+  std::cout << "Type a number: ";
+  int x;
+  std::cin >> x;
+  std::cout << "Your number is: " << x << std::endl;
+  system("pause");
+  if (x > 0) {
+    std::cout << "Your number is positive." << std::endl;
+  } else if (x < 0) {
+    std::cout << "Your number is negative." << std::endl;
+  } else {
+    std::cout << "Your number is zero." << std::endl;
+  }
+  if (x % 2 == 0) {
+    std::cout << "Your number is even." << std::endl;
+  } else {
+    std::cout << "Your number is odd." << std::endl;
+  }
   return 0;
 }
