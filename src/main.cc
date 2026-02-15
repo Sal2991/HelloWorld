67f282984b03a0a565dbc656da9a5e7e96c04356
Sal2991 <clongsong@gmail.com>
2026-02-14 22:19:56 -0500

    i hate this

--- src/main.cc
+++ src/main.cc
@@ -2,19 +2,26 @@
 
 
 int main() {
-    int dash;
-    std::cout << "number\n";
-    std::cin >> dash;
-    std::cout << "string\n";
-    std::string x = "";
-    std::cin >> x;
     int i;
-    for (i = 0; i < dash; i++){
-        std::cout << "-----------------------\n";
-        // could automate this to have customizable column amount.
-        std::cout << "||  " << x << "  ||  " << x << "  ||  "<< x << "  ||\n";
-    } if (i == dash){
-        std::cout << "-----------------------\n";
+    int row;
+    int column;
+    std::string y = "";
+    std::cout << "text\n";
+    std::cin >> y;
+    std::cout << "amount of columns\n";
+    std::cin >> row;
+    std::cout << "amount of rows\n";
+    std::cin >> column;
+    std::string x = "[ " + y + " ] ";
+    for(int k = 0; k < column; k++){
+        for(i = 0; i < row; i++){
+            std::cout << std::string(x.length() * row - 1, '-') + '\n';
+            for (int j = 0; j < row; j++){
+                std::cout << x;
+            }
+            std::cout << "\n";
+        }
     }
+    std::cout << std::string(x.length() * row - 1, '-') + '\n';
     return 0;
 }
