6f11ad8ed60ba98e8cf61f7528cd73c8edb9a132
Sal2991 <clongsong@gmail.com>
2026-02-21 16:55:34 -0500

    meh

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,15 @@
 #include <iostream>
 
+void getNum(){
+    int array[] = {};
+    for (int i = 0; i < 10; i++){
+        std::cout << "Please enter number " << i << "\n";
+        std::cin >> array[i];
+    }
+}
+
 
 int main() {
-    std::cout << "blank";
+    getNum();
     return 0;
 }
